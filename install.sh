0296e0acd2e8f32e7936ae88cb3e65d901f04654
Kyubai <mrkyubai@gmail.com>
2020-10-15 14:54:01 +0200

    automated backup

--- install.sh
+++ install.sh
@@ -2,3 +2,7 @@ pacman -Syu --noconfirm
 
 pacman -S net-tools base-devel linux-headers --noconfirm
 pacman -S seclists gobuster burpsuite hydra nmap netdiscover --noconfirm
+pacman -S docker --noconfirm
+
+# DO after install:
+# Setup Burp and Foxyproxy
