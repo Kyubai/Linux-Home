9f40403f9acd4f6f7dcba6ad902d192692b1004a
kyubai <mrkyubai@gmail.com>
2020-11-29 19:51:07 +0100

    new start

--- .gdbinit
+++
@@ -1,7 +0,0 @@
-# source ~/peda/peda.py
-# set disassembly-flavor intel
-# define hook-stop
-# x/i $rip
-# x/8wx $rsp
-# end
-source /tmp/pwndbg/gdbinit.py
--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
-pacman -Syu --noconfirm
+#!/bin/sh
 
-pacman -S net-tools base-devel linux-headers --noconfirm
-pacman -S seclists gobuster burpsuite hydra nmap netdiscover --noconfirm
-pacman -S docker docker-compose --noconfirm
+kernel=58
+
+pacman -Syu
+pacman -S net-tools base-devel --noconfirm
+pacman -S vim --noconfirm
+pacman -S virtualbox firefox linux$kernel-headers linux$kernel-virtualbox-host-modules virtualbox-host-dkms --noconfirm
+pacman -S keepass unzip ffmpeg v4l2loopback-dkms ethtool cryptosetup --noconfirm
 
 git config --global user.email "mrkyubai@gmail.com"
-git config --global user.name "Kyubai"
-
-# DO after install:
-# Setup Burp and Foxyproxy
+git config --global user.name "kyubai"
