f9dbf7bf4e9e8f4108a93b581496f98a18df94b9
kyubai <mrkyubai@gmail.com>
2020-12-11 11:05:33 +0100

    changes

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 #TODO:
 # uname -a for kernel version
 
+user_dir="/home/riegel"
 kernel="58"
 # pacman_vboxhost="virtualbox linux$kernel-virtualbox-host-modules virtualbox-host-dkms"
 pacman_vboxguest="virtualbox-guest-utils"
@@ -13,15 +14,18 @@ pacman_sectools="dc3dd dislocker libbde crackmapexec impacket bloodhound metaspl
 pacman -Syu --noconfirm
 
 # get blackarch repos
-cd ~/Downloads
-curl -O https://blackarch.org/strap.sh
-chmod +x strap.sh
-./strap.sh
+mkdir /root/Downloads
+curl -O https://blackarch.org/strap.sh --output-dir /root/Downloads
+chmod +x /root/Downloads/strap.sh
+/root/Downloads/strap.sh
 
-pacman -S $pacman_vboxhost $pacman_vboxguest $pacman_essentials $pacman_sectools --noconfirm
+pacman -S $pacman_vboxhost $pacman_vboxguest $pacman_essentials $pacman_sectools --noconfirm --needed
 
 git config --global user.email "mrkyubai@gmail.com"
 git config --global user.name "kyubai"
+
+cp ./i3config ~/.i3/config
+cp ./i3config $user_dir/.i3/config
 
 
 # TODO:
