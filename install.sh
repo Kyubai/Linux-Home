56fda90c71ca3e22fd1e11a79240b8b2000bc513
kyubai <mrkyubai@gmail.com>
2021-02-03 11:12:43 +0100

    updated

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@
 # uname -a for kernel version
 
 user_dir="/home/riegel"
-kernel="58"
+kernel="510"
 # pacman_vboxhost="virtualbox linux$kernel-virtualbox-host-modules virtualbox-host-dkms"
 pacman_vboxguest="virtualbox-guest-utils"
 pacman_essentials="net-tools base-devel vim linux$kernel-headers"
-pacman_linuxtools="firefox keepass ffmpeg zip unzip v412loopback-dmks ethtool pulseaudio pulseaudio-alsa pavucontrol proxychains chromium nautilus"
+pacman_linuxtools="firefox keepass ffmpeg zip unzip v412loopback-dmks ethtool pulseaudio pulseaudio-alsa pavucontrol proxychains chromium nautilus dnsmasq"
 pacman_sectools="dc3dd dislocker libbde crackmapexec impacket bloodhound metasploit hashcat seclists burpsuite libpff chisel nmap"
 
 pacman -Syu --noconfirm
