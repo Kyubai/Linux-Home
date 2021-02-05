e5245274dab7168b7d1be825030ad65094c9bb71
kyubai <mrkyubai@gmail.com>
2021-02-05 15:50:27 +0100

    updated

--- install.sh
+++ install.sh
@@ -2,13 +2,15 @@
 
 #TODO:
 # uname -a for kernel version
+# install intel-ucode or amd-ucode
 
 user_dir="/home/riegel"
 kernel="510"
 # pacman_vboxhost="virtualbox linux$kernel-virtualbox-host-modules virtualbox-host-dkms"
 pacman_vboxguest="virtualbox-guest-utils"
 pacman_essentials="net-tools base-devel vim linux$kernel-headers"
-pacman_linuxtools="firefox keepass ffmpeg zip unzip v412loopback-dmks ethtool pulseaudio pulseaudio-alsa pavucontrol proxychains chromium nautilus dnsmasq"
+pacman_linuxtools="firefox keepass ffmpeg zip unzip v412loopback-dmks ethtool pulseaudio pulseaudio-alsa pavucontrol proxychains chromium nautilus tcpdump"
+pacman_serverstuff="dnsmasq darkhttpd"
 pacman_sectools="dc3dd dislocker libbde crackmapexec impacket bloodhound metasploit hashcat seclists burpsuite libpff chisel nmap"
 
 pacman -Syu --noconfirm
@@ -19,7 +21,7 @@ curl -O https://blackarch.org/strap.sh --output-dir /root/Downloads
 chmod +x /root/Downloads/strap.sh
 /root/Downloads/strap.sh
 
-pacman -S $pacman_vboxhost $pacman_vboxguest $pacman_essentials $pacman_sectools --noconfirm
+pacman -S $pacman_vboxhost $pacman_vboxguest $pacman_essentials $pacman_sectools $pacman_serverstuff --noconfirm
 
 git config --global user.email "mrkyubai@gmail.com"
 git config --global user.name "kyubai"
