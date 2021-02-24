34751d4f20c50e222eed4b3ff740cd069a7be63c
kyubai <mrkyubai@gmail.com>
2021-02-24 14:32:43 +0100

    update

--- install.sh
+++ install.sh
@@ -9,14 +9,15 @@ config_dir="/home/riegel/linuxhome/config"
 kernel="510"
 pacman_vboxhost="virtualbox virtualbox-host-modules-arch"
 # pacman_vboxguest="virtualbox-guest-utils"
-pacman_essentials="net-tools base-devel vim linux-headers man-db dhcpcd dhclient ttf-fonts openssh"
-pacman_x="xorg xorg-xinit xterm i3-gaps dmenu i3status nitrogen"
-pacman_linuxtools="firefox keepass ffmpeg zip unzip v412loopback-dmks ethtool pulseaudio pulseaudio-alsa pavucontrol proxychains chromium nautilus tcpdump ranger keepassxc flameshot feh xfce4-power-manager htop"
+pacman_essentials="net-tools base-devel vim linux-headers man-db dhcpcd dhclient ttf-font openssh zsh yay networkmanager nm-connection-editor openvpn"
+pacman_x="xorg xorg-xinit xterm i3-gaps dmenu i3status nitrogen volumeicon i3lock network-manager-applet"
+pacman_linuxtools="firefox keepass ffmpeg zip unzip v412loopback-dmks ethtool pulseaudio-alsa proxychains chromium nautilus tcpdump ranger keepassxc flameshot feh xfce4-power-manager htop alsa-utils pulseaudio pavucontrol"
 pacman_serverstuff="dnsmasq darkhttpd"
 pacman_sectools="dc3dd dislocker libbde crackmapexec impacket bloodhound metasploit hashcat seclists burpsuite libpff chisel nmap recon-ng wpscan"
 pacman_optionals="libreoffice-fresh nextcloud-client discord"
 
 pacman -Syu --noconfirm
+# yay -Syu --noconfirm
 
 # get blackarch repos
 # mkdir -p /root/Downloads
@@ -24,7 +25,7 @@ pacman -Syu --noconfirm
 # chmod +x /root/Downloads/strap.sh
 # /root/Downloads/strap.sh
 
-pacman -S $pacman_vboxhost $pacman_vboxguest $pacman_essentials $pacman_sectools $pacman_serverstuff $pacman_x $pacman_optionals --noconfirm --needed
+pacman -S $pacman_vboxhost $pacman_vboxguest $pacman_essentials $pacman_x $pacman_linuxtools $pacman_sectools $pacman_serverstuff $pacman_x $pacman_optionals --noconfirm --needed
 
 git config --global user.email "mrkyubai@gmail.com"
 git config --global user.name "kyubai"
@@ -33,6 +34,8 @@ mkdir -p $user_dir/VBoxVMs
 mkdir -p $user_dir/Share
 mkdir -p $user_dir/.config
 mkdir -p $user_dir/.config/i3
+mkdir -p $user_dir/Apps
+mkdir -p /data/Server
 
 mkdir -p /root/.config
 mkdir -p /root/.config/i3
@@ -40,14 +43,14 @@ mkdir -p /root/.config/i3
 cp $config_dir/i3config $user_dir/.config/i3/config
 cp $config_dir/picom $user_dir/.config/picom.conf
 cp $config_dir/xinitrc $user_dir/.xinitrc
+cp $config_dir/profile $user_dir/.profile
 
 cp $config_dir/i3config ~/.config/i3/config
 cp $config_dir/picom /root/.config/picom.conf
+cp $config_dir/profile /root/.profile
 
 
 # TODO:
-# edit sudoers
-
+# https://github.com/laurent22/joplin/releases
 
 # change user
-# yay -S joplin
